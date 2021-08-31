9b32ca1f4b438e06e6a8b9e1cfeccbb6c4c6fe4b
t-naganuma <takuya.naganuma@gmail.com>
2021-08-31 09:41:03 +0900

    console.logを本番で削除 htmlをwebpackで出力するように変更

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,5 @@ import { add } from '../modules/math';
 console.log('app');
 
 const result = add(1, 2);
+
+// console.log(a);
--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/js/app.js',
+  entry: "./src/js/app.js",
   output: {
-    path: path.resolve(__dirname, 'public/js'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "public/"),
+    filename: "js/bundle.js",
   },
-}
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: "./src/html/index.html",
+      minify: false,
+    }),
+  ],
+};
