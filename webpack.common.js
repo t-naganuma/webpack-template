21965cfb48d55913eb7be51398da7ac84c6b1e65
t-naganuma <takuya.naganuma@gmail.com>
2021-09-14 20:52:35 +0900

    エントリーポイントを複数にする

--- webpack.common.js
+++ webpack.common.js
@@ -3,16 +3,26 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/js/app.js",
+  entry: {
+    app: "./src/js/app.js",
+    another: "./src/js/another.js",
+  },
   output: {
     path: path.resolve(__dirname, "public/"),
-    filename: "js/bundle.js",
+    filename: "js/[name].bundle.js",
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HTMLWebpackPlugin({
       template: "./src/html/index.html",
       minify: false,
+      chunks: ["app"],
+    }),
+    new HTMLWebpackPlugin({
+      filename: "another.html",
+      template: "./src/html/another.html",
+      minify: false,
+      chunks: ["another"],
     }),
   ],
 };
